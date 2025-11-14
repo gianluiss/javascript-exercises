ffbf8f636b7c1690f450a6080e7a94427538509c
luisito0028 <gianluistapia28@gmail.com>
2025-11-14 23:46:11 +0800

    Answered 15_GETTHETITLES

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,29 @@
-const getTheTitles = function() {
-
+// .map method
+const getTheTitles = function(books) {
+    return books.map( (book) => book.title );
 };
+
+/* .reduce method
+const getTheTitles = function(books) {
+    return books.reduce( (titles, book) => {
+        titles.push(book.title);
+        return titles;
+    }, []);
+}
+*/
+
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
+
+console.log( getTheTitles(books) );
 
 // Do not edit below this line
 module.exports = getTheTitles;
